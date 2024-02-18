6f61a3a9d12362fc76d9340c92ff256529d3a064
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-02-18 17:37:19 +0300

    add about.css, modify index.js for about

--- js/index.js
+++ js/index.js
@@ -39,3 +39,22 @@ ScrollReveal().reveal(".header__content .header__button", {
   ...scrollRevealOption,
   delay: 1000,
 });
+
+//about container
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .about__button", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
